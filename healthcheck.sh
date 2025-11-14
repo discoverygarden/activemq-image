af01ec7c0682ad9f9c00e87383aedeb1ddc90783
Alexander Cairns <alexandercairns@discoverygarden.ca>
2025-11-14 15:48:08 -0400

    cleanup

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# set -x
 export ACTIVEMQ_OPTS="${ACTIVEMQ_OPTS/"$JMX_OPT"}"
 bin/activemq query --objname type=Broker,brokerName=*,service=Health | grep -qE '^CurrentStatus *= *Good$'
 
